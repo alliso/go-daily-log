df735b03466a3190ed106ddffbe7d69e0cbea66a
Fernando Álvarez-Castellanos Rojo <fernando.alvarez@cttexpress.com>
2024-07-12 09:11:07 +0200

    feat(create-log): creating log repository

--- application/log_day.go
+++ application/log_day.go
@@ -3,9 +3,22 @@ package application
 import (
 	"fmt"
 	"go-daily-log/domain/log"
+	domain "go-daily-log/domain/repository"
+	infr "go-daily-log/infrastructure/db/sqlite/repository"
 	"time"
 )
 
-func LogDay(logEntry log.Entry) {
-	fmt.Println(logEntry.Date.Format(time.DateOnly), logEntry.Project, logEntry.Task, logEntry.Hours)
+type LogDayCommand struct {
+	entryRepository domain.EntryRepository
+}
+
+func LogDay() *LogDayCommand {
+	return &LogDayCommand{
+		entryRepository: infr.NewEntryRepositoryImpl(),
+	}
+}
+
+func (l *LogDayCommand) Apply(logEntry log.Entry) {
+	fmt.Println(logEntry.Date.Format(time.DateOnly), logEntry.Project, logEntry.Task)
+	l.entryRepository.Save(logEntry)
 }
